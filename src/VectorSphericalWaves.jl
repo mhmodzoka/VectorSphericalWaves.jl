52efb723ac5f09206c116873cecb6bf429e96d35
Mahmoud Elzouka <mhmodzoka@gmail.com>
2021-05-17 09:58:04 -0700

    autodiff works with separate real imag functions

--- src/VectorSphericalWaves.jl
+++ src/VectorSphericalWaves.jl
@@ -26,6 +26,8 @@ module VectorSphericalWaves
 using SpecialFunctions
 using DataStructures
 using StaticArrays
+using ComplexOperations
+
 
 import WignerD
 import ChainRulesCore
@@ -45,6 +47,10 @@ export P_mn_of_θ_ϕ
 export γ_mn
 export γ_mn_dash
 
+# exporting main functions, separating real and imaginary parts
+export M_mn_wave_SeparateRealImag
+export N_mn_wave_SeparateRealImag
+
 # exporting functions that calculate for all m,n combinations
 export M_N_wave_all_m_n
 export B_C_P_mn_of_θ_ϕ_for_all_m_n
@@ -60,8 +66,15 @@ export get_max_single_index_from_n_max
 export πₘₙ_τₘₙ_all_all_m_n
 
 #############################################################################################
+"""
+    Dirac delta function
+returns 1 if the two inputs are equal. Otherwise, return Zero
+"""
 δ(x,y) = ==(x, y)
 
+"""
+    Fuse the two indices (m,n) into a single index.
+"""
 function single_index_from_m_n(m, n)
     return n * (n + 1) + m
 end
@@ -104,7 +117,10 @@ ChainRulesCore.@scalar_rule(
 
 # Wigner-d
 """
-Wigner-d function calculated from eq. B.1
+    Wigner-d function calculated from eq. B.1
+This is calculated directly, not with recurrence relation.
+The Wigner-d calculated by recurrence is more numerically stable.
+Use this function only as a validation for the recurrence relation, or when automatic differentiation is needed.
 """
 function wignerdjmn_ELZOUKA(s, m, n, θ)
     # println("s=$s, m=$m, n=$n, θ=$θ")
@@ -616,6 +632,138 @@ function M_N_wave_all_m_n(n_max, kr, θ, ϕ; kind="regular")
 end
 
 
+#############################################################################################
+# The following functions are the same as before, but are desinged to be automatically differentiable (e.g., with Zygote).
+# The main difference is that we don't use any complex numbers here. Complex numbers and vectors are represented as 1x2 and nx2 Matrices, respectively. The first and second columns represent the real and imaginary parts.
+#############################################################################################
+#############################################################################################
+# calculate B(θ), C(θ), P(θ)
+function B_mn_of_θ_SeparateRealImag(m::Int, n::Int, θ::Real)
+    # equation C.19
+    B_real = vcat(0, τₘₙ(m, n, θ), 0)
+    B_imag = vcat(0, 0, πₘₙ(m, n, θ))
+    return hcat(B_real, B_imag)
+end
 
+function C_mn_of_θ_SeparateRealImag(m::Int, n::Int, θ::Real)
+    # equation C.20  
+    C_real = vcat(0, 0, -1 * τₘₙ(m, n, θ))
+    C_imag = vcat(0, πₘₙ(m, n, θ), 0)
+    return hcat(C_real, C_imag)
+end
 
+function P_mn_of_θ_SeparateRealImag(m::Int, n::Int, θ::Real)
+    # equation C.21
+    return hcat(P_mn_of_θ(m, n, θ), vcat(0, 0, 0))
+end
+
+function convert_from_fun_of_θ_to_fun_of_θ_ϕ(fun_tobe_converted::Function, m::Int, n::Int, θ::Real, ϕ::Real)
+    # equation C.16, C.17, and C.18
+    coeff = (-1)^m * sqrt(factorial(n + m) / factorial(n - m))
+    B_of_θ_coef = coeff .* fun_tobe_converted(m, n, θ)
+    B_of_θ_coef_real = B_of_θ_coef[:,1]
+    B_of_θ_coef_imag = B_of_θ_coef[:,2]
+    exp_imϕ_real = cos(m * ϕ)
+    exp_imϕ_imag = sin(m * ϕ)
+    """
+    # did't work
+    B_real = B_of_θ_coef_real .* real(exp(im * m * ϕ)) - B_of_θ_coef_imag .* real(exp(im * m * ϕ))
+    B_imag = B_of_θ_coef_real .* imag(exp(im * m * ϕ)) + B_of_θ_coef_imag .* real(exp(im * m * ϕ))
+    """
+    B_real = B_of_θ_coef_real .* exp_imϕ_real - B_of_θ_coef_imag .* exp_imϕ_imag
+    B_imag = B_of_θ_coef_real .* exp_imϕ_imag + B_of_θ_coef_imag .* exp_imϕ_real
+
+    return hcat(B_real, B_imag)
+end
+
+#############################################################################################
+# calculate B(θ,ϕ), C(θ,ϕ), P(θ,ϕ)
+function B_mn_of_θ_ϕ_SeparateRealImag(m::Int, n::Int, θ::Real, ϕ::Real)
+    # equation C.16
+    return convert_from_fun_of_θ_to_fun_of_θ_ϕ(B_mn_of_θ_SeparateRealImag, m, n, θ, ϕ)
+end
+
+function C_mn_of_θ_ϕ_SeparateRealImag(m::Int, n::Int, θ::Real, ϕ::Real)
+    # equation C.17
+    return convert_from_fun_of_θ_to_fun_of_θ_ϕ(C_mn_of_θ_SeparateRealImag, m, n, θ, ϕ)
+end
+
+function P_mn_of_θ_ϕ_SeparateRealImag(m::Int, n::Int, θ::Real, ϕ::Real)
+    # equation C.18
+    return convert_from_fun_of_θ_to_fun_of_θ_ϕ(P_mn_of_θ_SeparateRealImag, m, n, θ, ϕ)
+end
+
+#############################################################################################
+# calculate spherical Bessel and Hankel functions and their derivarive
+function SeparateRealImag_for_Bessel_Hankel_and_derivatives(fun_tobe_converted::Function, n::Int, x_r::Real, x_i::Real)
+    bessel_complex = fun_tobe_converted(n, complex(x_r, x_i))
+    return hcat(real(bessel_complex), imag(bessel_complex))
+end
+
+function spherical_Bessel_j_n_SeparateRealImag(n::Int, x_r::Real, x_i::Real)    
+    return SeparateRealImag_for_Bessel_Hankel_and_derivatives(spherical_Bessel_j_n, n, x_r, x_i)
+end
+
+function spherical_Bessel_y_n_SeparateRealImag(n::Int, x_r::Real, x_i::Real)    
+    return SeparateRealImag_for_Bessel_Hankel_and_derivatives(spherical_Bessel_y_n, n, x_r, x_i)
+end
+
+function spherical_Hankel_h1_n_SeparateRealImag(n::Int, x_r::Real, x_i::Real)
+    return SeparateRealImag_for_Bessel_Hankel_and_derivatives(spherical_Hankel_h1_n, n, x_r, x_i)
+end
+
+function one_over_x_by_∂_x_j_n_by_∂x_SeparateRealImag(n::Int, x_r::Real, x_i::Real)
+    # n_over_x = complex_divide(n * ones(size(x_r)), zeros(size(x_r)), x_r, x_i) # TODO: find a way to make sure all inputs have the same size
+    n_over_x = complex_divide(n, 0, x_r, x_i)
+    return (spherical_Bessel_j_n_SeparateRealImag(n - 1, x_r, x_i) - complex_multiply(n_over_x, spherical_Bessel_j_n_SeparateRealImag(n, x_r, x_i)))
+end
+
+function one_over_x_by_∂_x_y_n_by_∂x_SeparateRealImag(n::Int, x_r::Real, x_i::Real)
+    # n_over_x = complex_divide(n * ones(size(x_r)), zeros(size(x_r)), x_r, x_i) # TODO: find a way to make sure all inputs have the same size
+    n_over_x = complex_divide(n, 0, x_r, x_i)
+    return (spherical_Bessel_y_n_SeparateRealImag(n - 1, x_r, x_i) - complex_multiply(n_over_x, spherical_Bessel_y_n_SeparateRealImag(n, x_r, x_i)))
+end
+
+function one_over_x_by_∂_x_h_n_by_∂x_SeparateRealImag(n::Int, x_r::Real, x_i::Real)
+    der_j = one_over_x_by_∂_x_j_n_by_∂x_SeparateRealImag(n, x_r, x_i)
+    der_y = one_over_x_by_∂_x_y_n_by_∂x_SeparateRealImag(n, x_r, x_i)
+    return der_j + complex_multiply(0, 1, der_y[1], der_y[2])
+    # return (one_over_x_by_∂_x_j_n_by_∂x_SeparateRealImag(n, x_r, x_i) + complex_multiply(0, 1, one_over_x_by_∂_x_y_n_by_∂x_SeparateRealImag(n, x_r, x_i)...))
+end
+
+function get_radial_function_and_special_derivative_given_kind_SeparateRealImag(kind)
+    """
+    return the radial function that is appropriate with the type of VSWF
+    """
+
+    if kind in ["regular", "incoming", 1]
+        radial_function = spherical_Bessel_j_n_SeparateRealImag
+        radial_function_special_derivative = one_over_x_by_∂_x_j_n_by_∂x_SeparateRealImag
+    elseif kind in ["irregular", "outgoing", 2]
+        radial_function = spherical_Hankel_h1_n_SeparateRealImag
+        radial_function_special_derivative = one_over_x_by_∂_x_h_n_by_∂x_SeparateRealImag
+    else
+        throw(DomainError(""" 'kind' has to be one of the following: ["regular", "incoming", "irregular", "outgoing"] """))
+    end
+
+    return radial_function, radial_function_special_derivative
+end
+
+function M_mn_wave_SeparateRealImag(m::Int, n::Int, kr_r::Real, kr_i::Real, θ::Real, ϕ::Real, kind="regular")    
+    radial_function, _ = get_radial_function_and_special_derivative_given_kind_SeparateRealImag(kind)
+    gamma_by_radial = γ_mn(m, n) .* radial_function(n, kr_r, kr_i)
+    return complex_multiply(vcat(gamma_by_radial, gamma_by_radial, gamma_by_radial), C_mn_of_θ_ϕ_SeparateRealImag(m, n, θ, ϕ))
+    # TODO: write this in a more elegant way: vcat(gamma_by_radial, gamma_by_radial, gamma_by_radial)
+end
+
+function N_mn_wave_SeparateRealImag(m::Int, n::Int, kr_r::Real, kr_i::Real, θ::Real, ϕ::Real, kind="regular")
+    radial_function, radial_function_special_derivative  = get_radial_function_and_special_derivative_given_kind_SeparateRealImag(kind)
+    ceoff = complex_multiply(complex_divide(n * (n + 1), 0, kr_r, kr_i), radial_function(n, kr_r, kr_i))
+    rad_fun_der = radial_function_special_derivative(n, kr_r, kr_i)
+    return γ_mn(m, n) .* (
+        complex_multiply([ceoff; ceoff; ceoff], P_mn_of_θ_ϕ_SeparateRealImag(m, n, θ, ϕ))
+        + complex_multiply([rad_fun_der; rad_fun_der; rad_fun_der], B_mn_of_θ_ϕ_SeparateRealImag(m, n, θ, ϕ))
+    )
+    # TODO: write this in a more elegant way: [ceoff; ceoff; ceoff]
+end
 end # module
---
+++ test/test_auto_diff_separate_real_imag.jl
@@ -0,0 +1,52 @@
+# TODO:
+# 1- I need Zygote.jacobian and gradient return Zero for any argument of type "Int"
+
+# NOTE: Lessons learned:
+# 1- I should remove all kwargs to make gradients and Zygote.jacobian calculations smooth
+
+#############################################################################################
+using LinearAlgebra
+using SpecialFunctions
+using VectorSphericalWaves
+using ChainRulesCore
+using ComplexOperations
+
+import FiniteDifferences
+import Zygote
+
+#############################################################################################
+# testing automatic differenitation
+m, n, kr_r, kr_i, θ, ϕ = 1, 1, 1e5, 0, 0.2, 0.3
+kr = complex(kr_r, kr_i)
+
+Zygote.gradient(VectorSphericalWaves.πₘₙ, m, n, θ)
+Zygote.jacobian(VectorSphericalWaves.B_mn_of_θ_SeparateRealImag, m, n, θ)
+Zygote.jacobian(VectorSphericalWaves.C_mn_of_θ_SeparateRealImag, m, n, θ)
+Zygote.jacobian(VectorSphericalWaves.P_mn_of_θ_SeparateRealImag, m, n, θ)
+Zygote.jacobian(VectorSphericalWaves.P_mn_of_θ, m, n, θ) # and this works too
+
+Zygote.jacobian(VectorSphericalWaves.B_mn_of_θ_ϕ_SeparateRealImag, m, n, θ, ϕ)
+Zygote.jacobian(VectorSphericalWaves.C_mn_of_θ_ϕ_SeparateRealImag, m, n, θ, ϕ)
+Zygote.jacobian(VectorSphericalWaves.P_mn_of_θ_ϕ_SeparateRealImag, m, n, θ, ϕ)
+
+Zygote.jacobian(VectorSphericalWaves.spherical_Bessel_j_n_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.spherical_Bessel_y_n_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.spherical_Hankel_h1_n_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.one_over_x_by_∂_x_j_n_by_∂x_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.one_over_x_by_∂_x_y_n_by_∂x_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.one_over_x_by_∂_x_h_n_by_∂x_SeparateRealImag, n, kr_r, kr_i)
+
+Zygote.jacobian(VectorSphericalWaves.M_mn_wave_SeparateRealImag,m, n, kr_r, kr_i, θ, ϕ, "regular") # I can't add kwarg "kind". How can I add it?
+
+M_wave_calc_using_complex_numbers = M_mn_wave(m, n, complex(kr_r, kr_i), θ, ϕ; kind="regular")
+VectorSphericalWaves.M_mn_wave_SeparateRealImag(m, n, kr_r, kr_i, θ, ϕ, "regular") == hcat(real(M_wave_calc_using_complex_numbers), imag(M_wave_calc_using_complex_numbers))
+Zygote.jacobian(VectorSphericalWaves.M_mn_wave_SeparateRealImag,m, n, kr_r, kr_i, θ, ϕ, "regular") # I can't add kwarg "kind". How can I add it?
+
+N_wave_calc_using_complex_numbers = N_mn_wave(m, n, complex(kr_r, kr_i), θ, ϕ; kind="regular")
+VectorSphericalWaves.N_mn_wave_SeparateRealImag(m, n, kr_r, kr_i, θ, ϕ, "regular") == hcat(real(N_wave_calc_using_complex_numbers), imag(N_wave_calc_using_complex_numbers))
+Zygote.jacobian(VectorSphericalWaves.N_mn_wave_SeparateRealImag,m, n, kr_r, kr_i, θ, ϕ, "regular") # I can't add kwarg "kind". How can I add it?
